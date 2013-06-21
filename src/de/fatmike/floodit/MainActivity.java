6a7716275d72322c14b2733d780601b3471834c4
fatmike <fatmike303@gmail.com>
2013-06-21 17:04:27 +0200

    Source format cleanup

--- src/de/fatmike/floodit/MainActivity.java
+++ src/de/fatmike/floodit/MainActivity.java
@@ -3,13 +3,10 @@ package de.fatmike.floodit;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends Activity {
+	@Override
+	public void onCreate( final Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.main );
+	}
 }
