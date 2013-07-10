73eb77e25e4015f2b414038c732648bc331a0085
meonwax <meonwax@gmail.com>
2013-07-10 10:03:27 +0200

    Added listener interface with correct lifecycle management

--- src/de/meonwax/floodit/SettingsActivity.java
+++ src/de/meonwax/floodit/SettingsActivity.java
@@ -1,15 +1,36 @@
 package de.meonwax.floodit;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
 	@Override
 	public void onCreate( final Bundle savedInstanceState ) {
-
 		super.onCreate( savedInstanceState );
-
 		addPreferencesFromResource( R.xml.preferences );
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		PreferenceManager.getDefaultSharedPreferences( this ).registerOnSharedPreferenceChangeListener( this );
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		PreferenceManager.getDefaultSharedPreferences( this ).unregisterOnSharedPreferenceChangeListener( this );
+	}
+
+	@Override
+	public void onSharedPreferenceChanged( final SharedPreferences sharedPreferences, final String key ) {
+
+		if( key.equals( getString( R.string.pref_palette ) ) ) {
+			// TODO: Show toast message
+        }
 	}
 }
